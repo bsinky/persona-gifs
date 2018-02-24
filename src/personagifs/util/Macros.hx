ea51a384cd1ad36363e4e8e9f5c45492b44f3011
Benjamin Sinkula <bsinky@gmail.com>
2018-02-24 16:50:13 -0600

    Clean up macro error handling

--- src/personagifs/util/Macros.hx
+++ src/personagifs/util/Macros.hx
@@ -3,20 +3,22 @@ package personagifs.util;
 #if macro
 import haxe.macro.Context;
 import haxe.macro.Expr;
+import haxe.io.Path;
 import sys.FileSystem;
 #end
 
 class Macros {
     public static macro function getImages() {
         var imageDirectory = "img";
+        var absImageDir = Path.join([FileSystem.absolutePath("."), imageDirectory]);
 
-        if (!FileSystem.isDirectory(imageDirectory))
+        if (!FileSystem.exists(absImageDir) || !FileSystem.isDirectory(absImageDir))
         {
-            throw 'Yikes! Couldn\'t find directory ${imageDirectory}';
+            Context.fatalError('Couldn\'t find directory ${absImageDir}', Context.currentPos());
         }
 
         // make array of image names and convert to expressions
-        var exprs = [for(value in FileSystem.readDirectory(imageDirectory)) macro $v{value}];
+        var exprs = [for(value in FileSystem.readDirectory(absImageDir)) macro $v{value}];
 
         return macro $a{exprs};
   }
