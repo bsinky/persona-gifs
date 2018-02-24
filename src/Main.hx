8d2a21ebe66f57b85ab14de5bcec451a609ec197
Benjamin Sinkula <bsinky@gmail.com>
2018-02-24 17:15:03 -0600

    Finally displaying exactly how we want it

--- src/Main.hx
+++ src/Main.hx
@@ -3,6 +3,7 @@ package;
 #if js
 import js.Browser;
 import js.html.Element;
+import js.html.ImageElement;
 #end
 
 import Random;
@@ -13,13 +14,29 @@ class Main {
     public static var images:Array<String> = Macros.getImages();
     public static function main():Void {
         var containerElement:Element = cast Browser.document.querySelector(".personagif");
+        var imageElement:ImageElement = cast containerElement.querySelector("img");
 
-        containerElement.addEventListener("click", randomizeImage.bind(containerElement));
+        containerElement.addEventListener("click", randomizeImage.bind(imageElement));
 
-        randomizeImage(containerElement);
+        randomizeImage(imageElement);
+        resizeImage(imageElement);
+
+        Browser.window.addEventListener("resize", resizeImage.bind(imageElement));
     }
 
-    private static function randomizeImage(gifcontainer:Element) {
-        gifcontainer.style.backgroundImage = "url(\"img/" + Random.fromArray(images) + "\")";
+    private static function randomizeImage(imageElement:ImageElement) {
+        imageElement.src = "img/" + Random.fromArray(images);
+    }
+
+    private static function resizeImage(imageElement:ImageElement) {
+        imageElement.style.height = "";
+        imageElement.style.width = "";
+
+        if (Browser.window.innerHeight < Browser.window.innerWidth) {
+            imageElement.style.height = Browser.window.innerHeight + "px";
+        }
+        else {
+            imageElement.style.width = Browser.window.innerWidth + "px";
+        }
     }
 }
