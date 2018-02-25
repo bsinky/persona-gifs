d838cdf01caa566626b901d37b43963ef2cfa9df
Benjamin Sinkula <bsinky@gmail.com>
2018-02-25 09:12:23 -0600

    Add to and clean up tests

--- tests/TestMain.hx
+++ tests/TestMain.hx
@@ -5,11 +5,16 @@ using buddy.Should;
 
 class TestMain extends buddy.SingleSuite {
     public function new() {
-        describe("Utils", {
+        describe("Macros.getImages", {
             var macroImages:Array<String> = personagifs.util.Macros.getImages();
+            var filesystemImages = FileSystem.readDirectory("img");
 
-            it("getImages macro should contain all files from image directory", {
-                macroImages.should.containAll(FileSystem.readDirectory("img"));
+            it("should contain all files from image directory", {
+                macroImages.should.containAll(filesystemImages);
+            });
+
+            it("should contain the same number of images as the image directory", {
+                macroImages.length.should.be(filesystemImages.length);
             });
         });
     }
